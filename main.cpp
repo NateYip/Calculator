83e7f23089990b36998effcd6c91b9e821586c0c
nate yip <nateyip@natedeMacBook-Air.local>
2019-09-23 20:06:55 +0800

    fix multiplication

--- main.cpp
+++ main.cpp
@@ -11,10 +11,11 @@
 #include<algorithm>
 using namespace std;
 struct juzhen{
-    int len,wid;
+    int len,wid;//wid= 行，len= 列；
     float in[8][8];
 };//这是一个矩阵；
 
+float ans;//全局定义 mul2 中的数据
 struct juzhen a[8];//全局定义矩阵
 
 void input_a()//手动输入数据
@@ -26,9 +27,9 @@ void input_a()//手动输入数据
     for(int q=1;q<=n;q++)
     {
         cout<<"第"<<q<<"个矩阵的数据："<<endl;
-        cout<<"请输入它的长：";
+        cout<<"请输入它的列数：";
         cin>>a[q].len;
-        cout<<"请输入它的宽：";
+        cout<<"请输入它的行数：";
         cin>>a[q].wid;
         cout<<"请从左至右，再从上到下输入数据"<<endl;
         cout<<"输入将每个数据用空格隔开，每行结束后，请输入回车："<<endl;
@@ -93,19 +94,19 @@ void mul1()
 void mul2()
 {
     cout <<"以下是你输入矩阵与矩阵相乘的结果:"<<endl;
-    for (int i=1;i<=a[1].len;i++)
+    for (int i=1;i<=a[1].wid;i++)
     {
-        for(int j=1;j<=a[2].wid;j++)
+        for(int j=1;j<=a[2].len;j++)
         {
-            float ans =0;
-            for (int q=1;q<=a[1].wid;q++)
+            ans=0;
+            for (int q=1;q<=a[1].len;q++)
             {
                 ans=ans+a[1].in[i][q]*a[2].in[q][j];
-                printf("%-8.2f",ans);
-                cout <<' ';
                 
             }
-            cout <<endl;
+            printf("%-8.2f",ans);
+            cout <<' ';
+
         }
         cout <<endl;
     }
@@ -117,13 +118,13 @@ void mul2()
 int main()
 {
     char way;//这个值用来记录输入数据的方式
-    int how;//这个值用来记录计算类型
+    char how;//这个值用来记录计算类型
     
     cout<<"本程序是用来计算矩阵运算的，目前单个矩阵最大5*5，最多录入两个矩阵,计算结果精确度在小数点后两位。"<<endl;
-    cout<<"<!!!caution!!!：目前读取文件的数据的方式还在搭建,乘法也在调试>";
+    cout<<"<!!!caution!!!：目前读取文件的数据的方式还在搭建,乘法也在调试>"<<endl;
     cout <<"是否有需要手动输入文件(y=yes/n=no):";
     cin>>way;
-   
+   /*
     if (way == 'y')
         input_a();
     else input_b();
@@ -134,22 +135,25 @@ int main()
     if (how==1) add1();
     else if (how==2) mul1();
     else mul2();
+    */
     
-    
-    /*
+
      switch (way)
     {
-        case 'y':input_a;break;
-            //case 'n':input_b;break;通过文件读取不能用
+        case 'y':input_a();break;
+        case 'n':input_b();break;//通过文件读取不能用
     }
+    
+    cout<<"请输入计算方式，矩阵与矩阵相加请输入“1”，矩阵与实数相乘请输入“2”，矩阵与矩阵相乘请输入“3”：";
+    cin>>how;
     
     switch (how)
     {
-        case 1:add1;break;
-        case 2:mul1;break;
-        case 3:mul2;break;
+        case '1':add1();break;
+        case '2':mul1();break;
+        case '3':mul2();break;
     }
-    */
+
     
     cout <<"感谢使用"<<endl;
     return 0;
